af1f3963b0b1ba391740a6ad77427df11020c9ec
C.W. Betts <computers57@hotmail.com>
2019-07-04 17:27:37 -0600

    Update libebml and libmatroska.

--- Xcodes/ebml/EBMLConfigVersion.cmake
+++ Xcodes/ebml/EBMLConfigVersion.cmake
@@ -9,16 +9,16 @@
 # The variable CVF_VERSION must be set before calling configure_file().
 
 
-set(PACKAGE_VERSION "1.3.7")
+set(PACKAGE_VERSION "1.3.9")
 
 if(PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION)
   set(PACKAGE_VERSION_COMPATIBLE FALSE)
 else()
 
-  if("1.3.7" MATCHES "^([0-9]+)\\.")
+  if("1.3.9" MATCHES "^([0-9]+)\\.")
     set(CVF_VERSION_MAJOR "${CMAKE_MATCH_1}")
   else()
-    set(CVF_VERSION_MAJOR "1.3.7")
+    set(CVF_VERSION_MAJOR "1.3.9")
   endif()
 
   if(PACKAGE_FIND_VERSION_MAJOR STREQUAL CVF_VERSION_MAJOR)
